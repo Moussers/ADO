a744e025d64dab58607ab37f93530a6f8a8b7f10
Sand <sand999.com>
2026-03-19 02:41:10 +0300

    Add project files.

---
+++ ADO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //строка подключения
+            Console.WriteLine(connection_string);
+            SqlConnection connection = new SqlConnection(connection_string);    //подключение
+            connection.Open();
+            string cmd = "SELECT movie_id, title, release_date, first_name, last_name FROM Movies, Directors WHERE director = director_id";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            //прописывание и создание команды
+            SqlDataReader reader = command.ExecuteReader();
+            //выполнение команды
+            //ExecuteReader - вынимает таблицу, то есть набор значений
+            for (int i = 0; i < reader.FieldCount; i++) 
+            {
+                Console.Write(reader.GetName(i) + "\t");
+            }
+            Console.WriteLine();
+            while (reader.Read())       //Переход на следующую строку, пока строки не кончатся (NULL)
+            {
+                //Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
+                for (int i = 0; i < reader.FieldCount; ++i)     //Проход по столбцам и вывод информации с каждого из столбцов
+                {
+                    Console.Write($"{reader[i]}\t");        
+                }
+                Console.WriteLine();
+            }
+            reader.Close();
+            command.CommandText = "SELECT COUNT(*) FROM Movies";
+            //Замена текста команды используя функцию CommandText
+            Console.WriteLine($"Колличество записей:\t{command.ExecuteScalar()}");
+            //ExecuteScalar - вынимает одно значение, это и есть скалярный запрос
+            connection.Close();
+        }
+    }
+}
---
+++ ADO/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ADO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ADO")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("402423c4-8b70-4ce5-9e6e-4602ff8957df")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
