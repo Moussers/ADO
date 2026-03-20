087e0c342efd66b32b103414a833b13421e446d6
Sand <sand999.com>
2026-03-21 00:30:59 +0300

    Deleted unneded code from file as Program

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -10,7 +10,7 @@ namespace ADO
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             //строка подключения
             Connector connector = new Connector(connection_string);
-            connector.Insert("INSERT Directors (director_id, first_name,last_name) VALUES (12,N'Guy', N'Richie');");
+            connector.Insert("INSERT Directors (director_id, first_name, last_name) VALUES (12,N'Guy', N'Richie');");
             Console.WriteLine($"PK Max:\t{connector.GetMaxPrimaryKey("Directors")}");
             //string cmd = "SELECT movie_id, title, release_date, first_name, last_name FROM Movies, Directors WHERE director = director_id";
             //connector.Select(cmd);
@@ -23,14 +23,6 @@ namespace ADO
                 "director=director_id"
                 );
             Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
-            //string path = connection.Database;
-            //Console.WriteLine(path.ToString());
-            //string cmd = "SELECT movie_id, title, release_date, first_name, last_name FROM Movies, Directors WHERE director = director_id";
-            //SqlCommand command = new SqlCommand(cmd, connection);
-            //прописывание и создание команды
-            //outputInfoNotScalar(command);
-            //cmd = "Movies";
-            //outputInfoScalar(command, cmd);
         }
     }
 }
