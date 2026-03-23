7b4c4d1f0004c406fe447be9717d59cc11576adb
Sand <sand999.com>
2026-03-23 22:21:31 +0300

    GetPrimaryKeyColumnName

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlClient;
 
 namespace ADO
 {
@@ -10,7 +9,15 @@ namespace ADO
             string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             //строка подключения
             Connector connector = new Connector(connection_string);
-            connector.Insert("INSERT Directors (director_id, first_name,last_name) VALUES (12,N'Guy', N'Richie');");
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine(connector.GetPrimaryKeyColumnName("Directors"));
+            Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
+            //connector.Insert($@"INSERT Directors (director_id, first_name,last_name) VALUES ({connector.GetNextPrimaryKey("Directors")}, N'Guy', N'Richie');");
+            connector.Insert(
+                "Directors", 
+                "director_id,first_name,last_name",
+                $"{connector.GetNextPrimaryKey("Directors")},John,Singleton"
+                );
             Console.WriteLine($"PK Max:\t{connector.GetMaxPrimaryKey("Directors")}");
             //string cmd = "SELECT movie_id, title, release_date, first_name, last_name FROM Movies, Directors WHERE director = director_id";
             //connector.Select(cmd);
