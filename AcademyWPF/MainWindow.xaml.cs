92e0ae0c863601a9ce81cb54785a85b12762e82b
Sand <sand999.com>
2026-04-25 19:35:07 +0300

    Filters for students DONE

--- AcademyWPF/MainWindow.xaml.cs
+++ AcademyWPF/MainWindow.xaml.cs
@@ -38,7 +38,72 @@ namespace AcademyWPF
             tables[i].ItemsSource = connector.
                 Select($"SELECT * FROM {((sender as TabControl).Items[i] as TabItem).Header.ToString()}").DefaultView;
             statusBarCount.Text = $"Количество записей: {tables[i].Items.Count-1}";
+            filterByGroup.Items.Clear();
+            filterByDirection.Items.Clear();
+            if (tabControl.SelectedIndex == 0)
+            {
+                System.Data.DataTable grp = connector.
+                    Select($"SELECT group_name FROM Groups") ;
+                filterByGroup.Items.Add("Все группы");
+                for (int j = 0; j < grp.Rows.Count; j++)
+                    filterByGroup.Items.Add(grp.Rows[j][0]);
+                filterByGroup.SelectedIndex = 0;
+                System.Data.DataTable dir = connector.Select("SELECT direction_name FROM directions");
+                filterByDirection.Items.Add("Все направления");
+                for (int j = 0; j < dir.Rows.Count; ++j)
+                {
+                    filterByDirection.Items.Add(dir.Rows[j][0]);
+                }
+                filterByDirection.SelectedIndex = 0;
+            }
         }
+
+        private void change_sel(object sender, SelectionChangedEventArgs e)
+        {
+            if (tabControl.SelectedIndex == 0)
+            {
+                if ((filterByDirection.SelectedIndex <= 0) && (filterByGroup.SelectedIndex <= 0))
+                {
+                    tables[0].ItemsSource = connector.
+                        Select($"SELECT * FROM {(tabControl.Items[0] as TabItem).Header.ToString()}").DefaultView;
+                }
+                else if ((filterByDirection.SelectedIndex <= 0) && filterByGroup.SelectedIndex > 0)
+                {
+                    System.Data.DataTable grp_id = connector.
+                        Select($"SELECT group_id FROM groups WHERE group_name = '{filterByGroup.SelectedItem}'");
+                    tables[0].ItemsSource = connector.
+                        Select($"SELECT * FROM {(tabControl.Items[0] as TabItem).Header.ToString()}" +
+                        $" WHERE [group] = {grp_id.Rows[0][0]}").DefaultView;
+                }
+                else if ((filterByDirection.SelectedIndex > 0) && filterByGroup.SelectedIndex <= 0)
+                {
+                    System.Data.DataTable dir_id = connector.
+                        Select($"SELECT direction_id FROM directions WHERE direction_name = '{filterByDirection.SelectedItem}'");
+                    tables[0].ItemsSource = connector.
+                        Select($"SELECT * FROM {(tabControl.Items[0] as TabItem).Header.ToString()}" +
+                        $" INNER JOIN Groups on Groups.group_id = [group] WHERE direction = {dir_id.Rows[0][0]}").DefaultView;
+                }
+                else if ((filterByDirection.SelectedIndex > 0) && filterByGroup.SelectedIndex > 0)
+                {
+                    System.Data.DataTable dir_id = connector.
+                        Select($"SELECT direction_id FROM directions WHERE direction_name = '{filterByDirection.SelectedItem}'");
+                    System.Data.DataTable grp_id = connector.
+                        Select($"SELECT group_id FROM groups WHERE group_name = '{filterByGroup.SelectedItem}' AND direction = '{dir_id.Rows[0][0]}'");
+                    if (grp_id.Rows.Count != 0)
+                    {
+                        tables[0].ItemsSource = connector.
+                            Select($"SELECT * FROM {(tabControl.Items[0] as TabItem).Header.ToString()}" +
+                            $" WHERE [group] = {grp_id.Rows[0][0]}").DefaultView;
+                    }
+                    else
+                    {
+                        tables[0].ItemsSource = null;
+                    }
+                }
+                statusBarCount.Text = $"Количество записей: {tables[0].Items.Count - 1}";
+            }
+        }
+
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
